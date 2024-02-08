f2ee956c933bbabd20e8c3bc1ac27e5743a45e47
philip nguyen <nguyen.is.philip@gmail.com>
2024-02-08 02:18:04 -0500

    created collection and user models

---
+++ server/models/Collection.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CollectionSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    cards: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Card' 
+    }],
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Virtual for the URL of the collection
+CollectionSchema.virtual('url').get(function() {
+    return '/api/collections/' + this._id;
+});
+
+
+module.exports = mongoose.model('Collection', CollectionSchema);
